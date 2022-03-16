ef48ceb7a36a27cdeab553d059fed6cd595ec5be
HowardY <47439917+HowardY2@users.noreply.github.com>
2022-03-16 10:47:12 +0800

    Update ioda-env.sh

--- ioda-env.sh
+++ ioda-env.sh
@@ -4,6 +4,6 @@
 # ${IODA_EXP_DIR}/bin contains scripts and tools to automate IODA experiments
 
 
-export IODA_EXP_DIR="/home/huaicheng/proj/ioda/iodaExp"
+export IODA_EXP_DIR="/home/femu/myiodaExp"
 
 export PATH=${IODA_EXP_DIR}/bin:$PATH
